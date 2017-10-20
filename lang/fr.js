ce915f1026d39fdbf2ac2ac59272643aa435d6ba
MordecaiXLII <mordecaixlii@gmail.com>
2017-10-20 11:15:59 +0200

    Create fr.js
    French translation.

---
+++ lang/fr.js
@@ -0,0 +1,21 @@
+alert("French (en) loaded (by MordecaiXLII)");
+var STR_CHANNELS = "Chaînes";
+var STR_GAMES = "Jeux";
+var STR_OPEN = "Ouvrir";
+var STR_REFRESH = "Actualiser";
+
+var STR_PLACEHOLDER_OPEN = "Entrez le nom d'une chaîne...";
+
+var STR_QUALITY = "Qualité";
+
+var STR_RETRYING = "Nouvel essai";
+var STR_VIEWER = "Spectateurs";
+var STR_BUFFERING = "Buffering";
+
+
+var STR_ERROR_RENDER_SOURCE = "Format non supporté :(";
+var STR_ERROR_RENDER_FIXED = "Navré, mais il semblerait que les qualités\nHaute/Moyenne/Basse ne soient pas disponibles\nsur votre TV. Essayez la qualité Source.";
+var STR_ERROR_NETWORK_DISCONNECT= "Réseau déconnecté.";
+var STR_ERROR_STREAM_NOT_FOUND = "Stream non trouvé.";
+var STR_ERROR_AUTHENTICATION_FAIL = "L'authentification a échoué.";
+var STR_ERROR_CONNECTION_FAIL = "La connexion a échoué.";
